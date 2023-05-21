d23cda708ecfb14f86d2d407921cd20134d94522
Shin Soon <sweetejr777@gmail.com>
2023-05-22 01:22:29 +0900

    feat: Item Service create

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,4 +25,30 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+    /*
+     * 초기화
+     */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+    /*
+     * 재고취소
+     */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    /*
+     * 재고합조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
