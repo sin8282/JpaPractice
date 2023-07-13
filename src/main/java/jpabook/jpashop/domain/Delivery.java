62b65371d07760cdf9f68688e3f79ff97b98b7f3
Shin Soon <sweetejr777@gmail.com>
2023-07-14 01:23:25 +0900

    feat: OrderApiController v3.1 DTO + fetch join with Pagination

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -3,9 +3,11 @@ package jpabook.jpashop.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 
+@BatchSize(size = 100) // application.yml global value 말고 따로 설정하고 싶다면 여기다가. (Order.class 기준 ToOne일때만,)
 @Entity
 @Getter
 @Setter
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -95,7 +95,18 @@ public class OrderRepository {
     }
 
     public List<Order> findAllWithMemberDelivery() {
-        return em.createQuery("select o from Order o join fetch o.member m join fetch o.delivery d", Order.class)
+        return em.createQuery("select o from Order o " +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery("select o from Order o " +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
                 .getResultList();
     }
 
@@ -110,4 +121,6 @@ public class OrderRepository {
                         " join fetch oi.item i ", Order.class)
                 .getResultList();
     }
+
+
 }
